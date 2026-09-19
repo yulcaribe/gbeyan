7b498d3d46b5ed4dbe20d5dad15ac73b4a37357c
yulcaribe <mehmetisaacar47@gmail.com>
2026-09-19 07:29:32 +0300

    release api client v1.9.0

--- otobeyan/api.js
+++ otobeyan/api.js
@@ -1,7 +1,7 @@
 (function installOtoBeyanApi(global) {
   'use strict';
 
-  const CLIENT_VERSION = '1.8.9';
+  const CLIENT_VERSION = '1.9.0';
   const LOCAL_PRIMARY_API = 'https://gbeyan.yulcaribe.com';
   const LOCAL_FALLBACK_API = 'https://gbeyan.onrender.com';
   const IS_LOCAL_FILE = global.location?.protocol === 'file:';
